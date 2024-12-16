ade1c380e18587a859dc954b0fb16f25e5c4701e
Areli Cruz <arelycl17@gmail.com>
2024-12-15 23:46:15 -0600

    Delete sicaru directory

--- sicaru/src/main/java/org/stackmasters/sicaru/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package org.stackmasters.sicaru.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.stackmasters.sicaru.model.ProductEntity;
-
-public interface ProductRepository extends JpaRepository<ProductEntity,Long>{
-
-}
