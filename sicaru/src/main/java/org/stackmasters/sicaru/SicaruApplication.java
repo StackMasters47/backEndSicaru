a86b8a41fa98aab11e9175d0485583188b79695c
Irvingjmp <irvingjmp@gmail.com>
2024-12-15 22:29:58 -0600

    Configuración de Productos

---
+++ sicaru/src/main/java/org/stackmasters/sicaru/SicaruApplication.java
@@ -0,0 +1,13 @@
+package org.stackmasters.sicaru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SicaruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SicaruApplication.class, args);
+	}
+
+}
